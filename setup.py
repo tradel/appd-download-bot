c042069a0eb70488faaec11629078a820e7888e6
Todd Radel <tradel@appdynamics.com>
2015-06-24 17:27:40 -0400

    Bump version: 0.1.5 → 0.1.6

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*filenames, **kwargs):
     return sep.join(buf)
 
 setup(name='AppDynamicsDownloader',
-      version='0.1.5',
+      version='0.1.6',
       description='AppDynamics Download Robot',
       long_description=read('README.rst'),
       author='Todd Radel',
