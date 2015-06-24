00b00a0cbb30cda228845d30f257688843e7a2b3
Todd Radel <tradel@appdynamics.com>
2015-06-23 22:25:55 -0400

    First group of unit test cases

---
+++ test/test_urls.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import os
+from zone import Zone
+
+class UrlParsingTest(unittest.TestCase):
+
+    def setUp(self):
+        self.version = '4.1.0.0'
+        self.base_url = 'https://download.appdynamics.com/onpremise/public/archives'
+        self.zone = Zone(zone_id='on-premise',
+                         version=self.version,
+                         username=os.environ.get('APPD_SSO_USERNAME'),
+                         password=os.environ.get('APPD_SSO_PASSWORD'),
+                         use_cookies=False)
+
+    def test_controller_linux_64bit(self):
+        url = self.zone.get_url('controller', 'linux', '64')
+        self.assertEqual(url, '{0.base_url}/{0.version}/controller_64bit_linux-{0.version}.sh'.format(self))
+
+    def test_controller_linux_32bit(self):
+        url = self.zone.get_url('controller', 'linux', '32')
+        self.assertEqual(url, '{0.base_url}/{0.version}/controller_32bit_linux-{0.version}.sh'.format(self))
+
+    def test_controller_windows_64bit(self):
+        url = self.zone.get_url('controller', 'windows', '64')
+        self.assertEqual(url, '{0.base_url}/{0.version}/controller_64bit_windows-{0.version}.exe'.format(self))
+
+    def test_controller_windows_32bit(self):
+        url = self.zone.get_url('controller', 'windows', '32')
+        self.assertEqual(url, '{0.base_url}/{0.version}/controller_32bit_windows-{0.version}.exe'.format(self))
+
+    def test_controller_mac_64bit(self):
+        url = self.zone.get_url('controller', 'mac', '64')
+        self.assertEqual(url, '{0.base_url}/{0.version}/controller_64bit_mac-{0.version}.dmg'.format(self))
+
+    def test_controller_mac_32bit(self):
+        self.assertRaises(AssertionError, self.zone.get_url, 'controller', 'mac', '32')
+
