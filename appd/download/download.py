b81d5a5747c4f01a6b6797f3f5fbc606a416b9da
Todd Radel <tradel@appdynamics.com>
2015-09-10 12:24:55 -0400

    Added support for wider terminals

--- appd/download/download.py
+++ appd/download/download.py
@@ -3,43 +3,79 @@
 
 import sys
 import argparse
+import time
+
 from zone import Zone
+from term import get_terminal_size
+
+MAX_FILENAME_WIDTH = 32
+last_time = time.time()
+term_width = 80
 
 
+def size(nbytes):
+    """
+    Converts a raw file size to a human-readable representation, e.g.:
+    >>> size(1024 * 1024 * 4)
+    '  4.0MB'
+    >>> size(1024 * 1024 * 1024 * 3.5)
+    '  3.5GB'
+    :param nbytes: raw file size in bytes
+    :type nbytes: long
+    :return: formatted file size
+    :rtype: str
+    """
+    if nbytes >= 1024 * 1024 * 1024:
+        return '{0:5.1f}GB'.format(float(nbytes) / float(1024 * 1024 * 1024))
+    if nbytes >= 1024 * 1024:
+        return '{0:5.1f}MB'.format(float(nbytes) / float(1024 * 1024))
+    if nbytes >= 1024:
+        return '{0:5d}KB'.format(nbytes / 1024)
+
+
+# noinspection PyUnusedLocal
 def status_callback(src, status):
     if not args.quiet:
         print status + ' . . .',
         sys.stdout.flush()
 
 
+# noinspection PyUnusedLocal
 def success_callback(src):
     if not args.quiet:
         print
 
 
-def size(nbytes):
-    if nbytes >= 1024*1024*1024:
-        return '{0:5.1f}GB'.format(float(nbytes) / float(1024*1024*1024))
-    if nbytes >= 1024*1024:
-        return '{0:5.1f}MB'.format(float(nbytes) / float(1024*1024))
-    if nbytes >= 1024:
-        return '{0:5d}KB'.format(nbytes / 1024)
-
-
+# noinspection PyUnusedLocal
 def save_callback(src, eargs):
     if not args.quiet:
         pct = int(float(eargs['bytes_read']) / float(eargs['total_bytes']) * 100.0)
         kbps = long(float(eargs['bytes_read']) / (eargs['now_time'] - eargs['start_time']))
-        bar = '=' * (pct / 4)
 
-        print '\r{0:<20} [{1:<25}] {2:3d}% | {3:>6} | {4:>6}ps'.format(eargs['filename'], bar, pct,
-                                                                       size(eargs['bytes_read']), size(kbps)),
+        global last_time
+        if eargs['now_time'] - last_time < 1.0 and eargs['bytes_read'] < eargs['total_bytes']:
+            return
+        last_time = eargs['now_time']
+
+        fname_width = (term_width - (4 + 4 + 3 + 8 + 3 + 8 + 2)) / 2
+        bar_width = fname_width
+        if fname_width > MAX_FILENAME_WIDTH:
+            bar_width += fname_width - MAX_FILENAME_WIDTH
+            fname_width = MAX_FILENAME_WIDTH
+
+        bar = '=' * (pct * bar_width / 100)
+
+        fmt_string = '\r{0:<' + str(fname_width) + '} [{1:<' + str(bar_width) + '}] {2:3d}% | {3:>8} | {4:>6}ps'
+
+        print fmt_string.format(eargs['filename'], bar, pct,
+                                size(eargs['bytes_read']), size(kbps)),
+
         sys.stdout.flush()
 
-        if eargs['bytes_read'] == eargs['total_bytes']:
-            if not args.quiet:
-                print
-            print '{0} saved to {1}'.format(eargs['filename'], eargs['target'])
+    if eargs['bytes_read'] == eargs['total_bytes']:
+        if not args.quiet:
+            print
+        print '{0} saved to {1}'.format(eargs['filename'], eargs['target'])
 
 
 def parse_args():
@@ -113,6 +149,9 @@ def main():
     Zone.status += status_callback
     Zone.success += success_callback
     Zone.download_progress += save_callback
+
+    global term_width
+    term_width = get_terminal_size()[0]
 
     global args
     args = parse_args()
