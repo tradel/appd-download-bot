85dd9b9fb3e9439a067228a88b279b47ed1efab8
Todd Radel <tradel@appdynamics.com>
2015-09-10 12:25:09 -0400

    Bump version: 0.1.6 → 0.1.7

--- README.rst
+++ README.rst
@@ -2,7 +2,7 @@
 AppDynamics Download Robot
 ==========================
 
-Current version: 0.1.6
+Current version: 0.1.7
 Released: 23-Jun-2015
 
 .. image:: https://img.shields.io/travis/tradel/appd-download-bot.svg
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*filenames, **kwargs):
     return sep.join(buf)
 
 setup(name='AppDynamicsDownloader',
-      version='0.1.6',
+      version='0.1.7',
       description='AppDynamics Download Robot',
       long_description=read('README.rst'),
       author='Todd Radel',
